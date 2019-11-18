18b86ea89934ff68057dea9b2383ef0ac50ccb00
xiahf-wkw <57082744+xiahf-wkw@users.noreply.github.com>
2019-11-18 17:22:13 +0800

    添加普通升级方法

---
+++ port_method/normaldiff_method.py
@@ -0,0 +1,18 @@
+import requests
+import unittest
+from updata_port.port import *
+
+class ptsj(unittest.TestCase):
+    def test1_port200(self):
+        """测试接口是否200"""
+        normaldiff = normaldiffapiv3.json()
+        self.assertEqual(201,normaldiff['code'])
+    def test2_msg(self):
+        normaldiff = normaldiffapiv3.json()
+        if self.assertEqual(201,normaldiff['code']):
+            return ( '{}'.format(normaldiff['msg']))
+        else:
+            return ( '接口请求失败')
+if __name__ == '__main__':
+    unittest.main()
+
