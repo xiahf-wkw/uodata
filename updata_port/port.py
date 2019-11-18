0854b755f4a9ce7ba42ebe0f7c786e9df0e5b572
xiahf-wkw <57082744+xiahf-wkw@users.noreply.github.com>
2019-11-18 15:49:32 +0800

    添加接口

---
+++ updata_port/port.py
@@ -0,0 +1,50 @@
+import requests
+
+
+
+normaldiffapiv3 = requests.post(url='http://update.app.2345.com/backend/index.php?r=NormalDiffApiV3&&test',
+                                data={
+                                    'authkey':'f8b446102d719202098751b3326ca138',
+                                      'data':'HRoXR1FEbkZXFkRYVlwSCBIBFgcXHUBGXVtyBhdYUF1EAkBXAwUIAFZWVgEBVgMLAQ1dVgVQB'
+                                             'AoFVw5aVwcCXAVeABYYFFRIRkYNE0JuRQIACwpYR1gFWl1tVQsAX11dCmQXBAQEAwpuEQcBCw'
+                                             'BDUR1YXFxGWRdSRFdfPAJBV2ceVAFoQQYBAgBIa0QJAgIARVBca0ABUgEBCGoWUQEBCl4OWgE'
+                                             'DBQEEBlQDBg0ACG5FCQgFB0xAHxFdRTwXVEFLD1cMFg4UCBIeRlRZWFxeV1wbAhVGUk9SV1lF'
+                                             'CxRQRF0PZwFEUGlJXFFGGxNWXlRtXV0NFQ8TUQoABAZUAFNRC1JeAABXBggDUVUBBFtUBlAFD'
+                                             'g0BU1RAHxFGTxMEEwkaE0gGVUBTExwQElJDSltfXBIDGgYFBUAfEUJeDA9UbFUJXAdYFgwTeH'
+                                             '8lGnB1AgAQHBtNR1FQFlZyQkY1BENAUQlWQA4WBR8AEEgVQl1Zb0RVS0teWl9ACREDGFJPARE'
+                                             'UREgDV19XVlVtClZcXBAKEFNWVRlBWANdQlsEUFUEERREWRJEX1NIEghGUlAPVwkEBgoOAFdX'
+                                             'UFUEAQFRWAgEXVILVANSUAkHCgcVHRtcVUZHVkpcakUbQ1YQDEEWWFVRRBRAUF1bVF5BDVhfGw'
+                                             'gSSWxMCAcHAwteQFtqFlEBAQpcZBcEBAQDBARUBwYIAAIFAAgLBwwCPkYDAgRRTW1GCFYKUER'
+                                             'cWV9VbQZFUFdWbEcACQoFD20XAwMABCs0cGR9L2QXBAQEAxxuEQcBCwBRXFRLV15RbgtXb0cGU'
+                                             '1MDCWQTCFIGBgMHCApWVQgNC1EHAl9dD1NtFwMDAARPPUQDCFQKF0FdUm1FAlQFAwNuRQIACwp'
+                                             '+T30kBnBFRCgxfAJALlVHBnZXcn9FEG5lUwEVAHZtQHFTXTRBVGB3VhVQWWtUWTZ6WGJHcn8p'
+                                             'b3RKYglfVWhpRARrMgdyfFsaUWRZAFcKEwJnZRQCcChhQxwAdlhxHAtzEAImb0YCBlFTHW9NV'
+                                             'ghQBllXUmxHVAcDCwhsRwAJCgUNCVMDC1RVW1EEBQw6TVIEBgQdbEdUBwMLXUNtRlxKRFxeDGx'
+                                             'QXVIGPUQDCFQKWGhBBgECAFYPbUwCAAACRBpK'
+                                      }
+                                )
+
+
+hotapiv3 = requests.post(url='http://update.app.2345.com/backend/index.php?r=HotApiV3',
+                         data={
+                                'uniDevice':'3ad99fb1846d48abf2f9986ac66576d3',
+                                'authkey':	'demo',
+                                'hot_version_name':	'none',
+                                'magician_version_name':	'3.1.3',
+                                'runtime_env':	'0',
+                                'hot_version_code':	'0',
+                                'os_version':	'28',
+                                'channel':	'unknow',
+                                'sign':	'VwxUVQBWUFIMBlBVUAZUVVcNDFEEAgEJVFBSUFwNXQg=',
+                                'magician_id':	'3085785198e7039326052c9ab3f43f94',
+                                'app_file_md5':	'f1880148f542cb66eb5bcd0b0acf08a7',
+                                'magician_version_code':	'5',
+                                'origin_channel':	'',
+                                'phone_model':	'OPPO_PCAM10',
+                                'uid':	'A00000A1F17A14',
+                                'app_version_code':	'13',
+                                'package_name':	'com.mobile2345.demo',
+                                'appkey':	'cbbecda54c678a8442d4bbf8016af923',
+                                'app_version_name':	'3.0.13',
+                                'network_type':	'wifi'
+                         })
